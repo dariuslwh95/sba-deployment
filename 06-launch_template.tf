c2c4bc9478407c879e0bcfd451ba5ab770094041
dariuslwh95 <darius.lwh@gmail.com>
2022-07-19 16:22:14 +0800

    refine
    refine

--- 06-launch_template.tf
+++ 06-launch_template.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "lambda-lt" {
   name_prefix   = "lambda-lt"
-  image_id      = "ami-0a66e99234c17ef87"
+  image_id      = "ami-0dc633995664f082b"
   instance_type = "t3a.small"
   user_data = base64encode(templatefile("react-launch.sh", { api_endpoint = "${aws_lb.lambda-lb-api.dns_name}" }))
   key_name = "lambda-key-pair"
@@ -12,8 +12,8 @@ resource "aws_launch_template" "lambda-lt" {
 
 resource "aws_launch_template" "lambda-lt-apiserver" {
   name_prefix   = "lambda-lt"
-  image_id      = "ami-0a66e99234c17ef87"
-  instance_type = "a.small"
+  image_id      = "ami-0dc633995664f082b"
+  instance_type = "t3a.small"
   
 user_data = base64encode(templatefile("apiserver-launch.sh", { rds_endpoint = "${aws_db_instance.postgres.endpoint}" }))
   key_name = "lambda-key-pair"
--- apiserver-launch.sh
+++ apiserver-launch.sh
@@ -6,6 +6,3 @@ yum install -y awslogs
 systemctl enable awslogsd 
 systemctl start awslogsd
 docker run -d --log-driver=awslogs --log-opt awslogs-region=ap-south-1 --log-opt awslogs-group=lambda --log-opt awslogs-create-group=true -p 80:8080 --env spring.datasource.username=postgres --env spring.datasource.password=postgres --env spring.datasource.url=jdbc:postgresql://${rds_endpoint}/smartbankapp brainupgrade/sba-apiserver:usage
-
-# docker run -d -p 80:3000 --env REACT_APP_API_URL=http:// anshshrivastava/smartbankfrontend:v4
-# docker run -d --log-driver=awslogs --log-opt awslogs-region=ap-south-1 --log-opt awslogs-group=lambda-apiserver --log-opt awslogs-create-group=true -p 8080:8080 --env spring.datasource.username=postgres --env spring.datasource.password=postgres --env spring.datasource.url=jdbc:postgresql://ansh-smartbank-db.cahc0qobfhru.ap-south-1.rds.amazonaws.com:5432/smartbankapp anshshrivastava/smartbankapi:v5
