ae875fa52e662b92a2b655e0e5fef56c94ee544a
dariuslwh95 <darius.lwh@gmail.com>
2022-07-19 16:47:27 +0800

    refine
    refine

--- 06-launch_template.tf
+++ 06-launch_template.tf
@@ -14,8 +14,7 @@ resource "aws_launch_template" "lambda-lt-apiserver" {
   name_prefix   = "lambda-lt-api"
   image_id      = "ami-0dc633995664f082b"
   instance_type = "t3a.small"
-  
-user_data = base64encode(templatefile("apiserver-launch.sh", { rds_endpoint = "${aws_db_instance.postgres.endpoint}" }))
+  user_data = base64encode(templatefile("apiserver-launch.sh", { rds_endpoint = "${aws_db_instance.postgres.endpoint}" }))
   key_name = "lambda-key-pair"
   vpc_security_group_ids = ["${aws_security_group.lambda-vpc-sg.id}"]
   iam_instance_profile {
